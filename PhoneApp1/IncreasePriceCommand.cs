83c227e9325b2f17d2236ed3c958f3c731954479
Студент <@DESKTOP-H16T94L>
2023-01-24 11:05:10 +0300

    Добавьте файлы проекта.

---
+++ PhoneApp1/IncreasePriceCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PhoneApp1
+{
+    public class IncreasePriceCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        PhoneViewModel viewModel;
+        public IncreasePriceCommand(PhoneViewModel vm)
+        {
+            viewModel = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return viewModel.Price < 100000;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+                viewModel.IncreasePrice();
+        }
+    }
+}
---
+++ PhoneApp1/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace PhoneApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+        InitializeComponent();
+        this.BindingContext = new PhoneViewModel
+        {
+            Title = "iPhone 7",
+            Company = "Apple",
+            Price = 52000
+        };
+    }
+}
+
